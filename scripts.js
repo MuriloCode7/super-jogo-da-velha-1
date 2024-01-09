a1162e7b20b9039f81419c4f4df95f6242aac5f4
MuriloCode7 <murilomagalhaes60@gmail.com>
2024-01-09 09:56:26 -0300

    highlight cells

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 function drawGame() {
+  var id = 0;
   for (var x = 0; x < 3; x++) {
     var row = document.createElement("div");
     row.className = "row";
     for (var y = 0; y < 3; y++) {
+      id++;
       var cell = document.createElement("div");
-      cell.className = "cell";
+      cell.classList.add("cell");
+      cell.id = id.toString();
       if (y !== 2) {
         cell.style.borderRight = "solid black 5px";
       }
@@ -19,15 +22,15 @@ function drawGame() {
 }
 
 function drawMiniGame(source) {
-
+  var id = 0;
   for (var x = 0; x < 3; x++) {
-
     var row = document.createElement("div");
     row.className = "miniRow";
     for (var y = 0; y < 3; y++) {
-
+      id++;
       var cell = document.createElement("div");
-      cell.className = "miniCell";
+      cell.classList.add("miniCell");
+      cell.id = id.toString();
       if (y !== 2) {
         cell.style.borderRight = "solid black 2px";
       }
@@ -35,11 +38,49 @@ function drawMiniGame(source) {
         cell.style.borderBottom = "solid black 2px";
       }
       row.appendChild(cell);
-
     }
     source.appendChild(row);
   }
-
 }
 
+function click(div) {
+  div.addEventListener("click", function () {
+    if (!div.classList.contains("selected")) {
+      count++;
+      var simbol = document.createElement("span");
+      if (count % 2 !== 0) {
+        simbol.textContent = "X";
+        simbol.style.color = "blue";
+      } else {
+        simbol.textContent = "O";
+        simbol.style.color = "red";
+      }
+      div.appendChild(simbol);
+      div.classList.add("selected");
+      highlightCell(div.id);
+    }
+  });
+}
+
+function highlightCell(id) {
+  for (const cell of cells) {
+    if (cell.id === id) {
+      cell.classList.add("highlightedCell");
+    } else {
+      cell.classList.remove("highlightedCell");
+    }
+  }
+}
+
+// Desenha o jogo
 drawGame();
+
+var cells = document.querySelectorAll(".cell");
+
+// Atribui a cada mini célula a função de click
+var miniCells = document.querySelectorAll(".miniCell");
+miniCells.forEach(function (div) {
+  click(div);
+});
+
+var count = 0;
